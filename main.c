5b2867d63b71e857312d59a48e8b0fc3926944c1
Dylan Shaffer <swimdoctor5@gmail.com>
2025-11-08 19:13:56 -0500

    Added Billboards

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include "world.h"
 
 // ASSETS
 #define X(name, path) Image name;
@@ -17,12 +18,13 @@ loadass() {
 }
 
 const int
-GAMEW = 320,
-GAMEH = 180;
+GAMEW = 160,
+GAMEH = 90;
 
 #define GAMEW 320
 #define GAMEH 180
 #define ASPECT (float)GAMEW / (float)GAMEH
+#define TREESCALE 8
 
 const Color 
 sky = {100, 100, 255, 255};
@@ -56,12 +58,76 @@ struct {
     float roll;
     float fov;
     float foc_len;
-} 
+}
 cam = {
     .pos = {0,0,5},
     .frwd = {1,0,0},
     .fov = PI / 2,
 };
+
+void 
+printv3(Vector3 vec) {
+    printf("(%f, %f, %f)\n", vec.x, vec.y, vec.z);
+}
+
+void
+drawbillboard(Vector3 billpos, Color* image) {
+    Quaternion rotate = QuaternionFromAxisAngle((Vector3) {0, 1, 0}, PI/2);
+    rotate = QuaternionMultiply(QuaternionFromAxisAngle((Vector3) {1, 0, 0}, -Vector2Angle((Vector2){1, 0}, (Vector2) {billpos.x - cam.pos.x, billpos.y - cam.pos.y})), rotate);
+    Vector3 localcampos = Vector3RotateByQuaternion(cam.pos, rotate);
+    localcampos = Vector3Add(localcampos, Vector3RotateByQuaternion(Vector3Scale(billpos, -1), rotate));
+    Vector3 localcamfrwd = Vector3RotateByQuaternion(cam.frwd, rotate);
+    Vector3 localcamside = Vector3RotateByQuaternion(cam.side, rotate);
+    Vector3 localcamup = Vector3RotateByQuaternion(cam.up, rotate);
+
+    // printf("Transformed Vectors:\n");
+    // printv3(localcampos);
+    // printv3(cam.frwd);
+    // printv3(localcamfrwd);
+    // printv3(localcamside);
+    // printv3(localcamup);
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+    //ToggleFullscreen();
+
+    for (int x = 0; x < GAMEW; x++) {
+        for (int y = 0; y < GAMEH; y++) {
+            Vector2 uv = {(float)x / GAMEW * 2 - 1, (float)y / GAMEH * 2 - 1};
+            uv.x *= ASPECT;
+            uv.y *= -1;
+
+            Vector3 ray = localcamfrwd;
+
+            ray = Vector3Add(ray, Vector3Scale(localcamside, uv.x * cam.foc_len));
+            ray = Vector3Add(ray, Vector3Scale(localcamup  , uv.y * cam.foc_len));
+            if(cam.roll != 0)
+                ray = Vector3RotateByAxisAngle(ray, localcamfrwd, cam.roll);
+            
+            if (ray.z >= -0.0f) {
+                continue;
+            }
+
+            ray = Vector3Scale(ray, -localcampos.z / ray.z);
+            ray = Vector3Add(ray, localcampos);
+
+            
+            int imgx = (int)(ray.y * TREESCALE) + asset_eviltree.width / 2;
+            int imgy = asset_eviltree.height - (int)(ray.x * TREESCALE);
+
+            if(imgx < 0 || imgx >= asset_eviltree.width) continue;
+
+            if(imgy < 0 || imgy >= asset_eviltree.height) continue;
+
+            Color imgCol = image[imgy * asset_eviltree.width + imgx];
+
+            if(imgCol.a == 0) continue;
+            if(1000/Vector3LengthSqr(ray) < depth[y][x]) continue;
+
+            depth[y][x] = 1000/Vector3LengthSqr(ray);
+            billpix[y][x] = imgCol;
+        }
+    }
+}
+
 
 void
 drawground() {
@@ -76,8 +142,8 @@ drawground() {
             Vector3 ray = cam.frwd;
             ray = Vector3Add(ray, Vector3Scale(cam.side, uv.x * cam.foc_len));
             ray = Vector3Add(ray, Vector3Scale(cam.up  , uv.y * cam.foc_len));
-            ray = Vector3RotateByAxisAngle(ray, cam.frwd, cam.roll);
-            ray = Vector3Normalize(ray);
+            if(cam.roll != 0)
+                ray = Vector3RotateByAxisAngle(ray, cam.frwd, cam.roll);
             
             if (ray.z >= -0.0f) {
                 grndpix[y][x] = sky;
@@ -105,17 +171,18 @@ docam() {
     cam.frwd.y = hro.mom.y;
 
     cam.frwd = Vector3Normalize(cam.frwd);
+    if(Vector3Equals(cam.frwd, Vector3Zero())) cam.frwd = (Vector3) {1, 0, 0};
     cam.side = Vector3CrossProduct((Vector3){0, 0, 1}, cam.frwd);
     cam.up   = Vector3CrossProduct(cam.frwd, cam.side);
     
     cam.foc_len = 1.0 / tan(cam.fov / 2.0);
 
-    printf("pos = (%f, %f)\n", cam.pos.x, cam.pos.y);
+    //printf("pos = (%f, %f)\n", cam.pos.x, cam.pos.y);
 }
 
 #define HRO_SZ           16
 #define HRO_TURNACC      0.3
-#define HRO_ACC          0.5
+#define HRO_ACC          0.15
 #define HRO_LINDAMP      0.98
 #define HRO_TIMETOUCH    3
 #define HRO_TIMETOUCH1   20
@@ -181,20 +248,49 @@ main(void) {
 
     InitWindow(800, 450, "raylib [core] example - basic window");
 
+    Color* treecol = LoadImageColors(asset_eviltree);
+
     Image img = GenImageColor(GAMEW, GAMEH, BLANK);
+
+    
     Texture2D grndtex = LoadTextureFromImage(img);
     Texture2D billtex = LoadTextureFromImage(img);
     Texture2D parttex = LoadTextureFromImage(img);
 
-    SetTargetFPS(60);
+    float billboardPos = 0;
+
+    //SetTargetFPS(60);
     while (!WindowShouldClose()) {
         dohro();
         docam();
         
+        if(IsKeyDown(KEY_F)) billboardPos -= 0.1f;
+        if(IsKeyDown(KEY_H)) billboardPos += 0.1f;
+
+        //Clear pixel buffers
+
+        
+        for (int x = 0; x < GAMEW; x++) {
+            for (int y = 0; y < GAMEH; y++) {
+                billpix[y][x].a = 0;
+                partpix[y][x].a = 0;
+                grndpix[y][x].a = 0;
+                depth[y][x] = 0;
+            }
+        }  
+
+        //draw trees
+        int index = 0;
+        do{
+            drawbillboard((Vector3) {treeloc[index].x, treeloc[index].y, 0}, treecol);
+            index++;
+        }
+        while(!Vector2Equals(treeloc[index], Vector2Zero()));
+        
         drawground();
-        UpdateTexture(grndtex, grndpix);
         UpdateTexture(billtex, billpix);
         UpdateTexture(parttex, partpix);
+        UpdateTexture(grndtex, grndpix);
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
@@ -208,10 +304,12 @@ main(void) {
                 tint = BLUE;
             Rectangle gamerect = (Rectangle){0,0,GAMEW,GAMEH};
             Rectangle winrect  = (Rectangle){0,0,GetScreenWidth(),GetScreenHeight()};
-            //DrawTexturePro(parttex, gamerect, winrect, Vector2Zero(), 0, tint);
-            //DrawTexturePro(billtex, gamerect, winrect, Vector2Zero(), 0, tint);
             DrawTexturePro(grndtex, gamerect, winrect, Vector2Zero(), 0, tint);
+            DrawTexturePro(billtex, gamerect, winrect, Vector2Zero(), 0, tint);
+            //DrawTexturePro(parttex, gamerect, winrect, Vector2Zero(), 0, tint);
         EndDrawing();
+
+        printf("%i\n", GetFPS());
     }
 
     CloseWindow();
